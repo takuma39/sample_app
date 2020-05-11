793526576b61f964729b5bf0142d4ff5c150b62d
vagrant <vagrant@localhost.localdomain>
2020-05-11 05:38:49 +0000

    [Finish]3-8 & Add application_controller_renderer.rbにシークレットキーを追加

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+#
+Refile.secret_key = '2a7f1fbf20f43e13dbf5133932925c7350211f717215b4437e390c7502ea34157cb94ed0fe0444ea1b02817997434c1114a61dce2ee50d16b095230a9f751ced'
