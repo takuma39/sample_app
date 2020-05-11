e996826ae87885b46f3dc5212fee2390d014fa07
vagrant <vagrant@localhost.localdomain>
2020-05-11 04:20:35 +0000

    Add bundle install & image_idカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_10_074450) do
+ActiveRecord::Schema.define(version: 2020_05_11_041730) do
 
   create_table "lists", force: :cascade do |t|
     t.string "title"
     t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_id"
   end
 
 end
